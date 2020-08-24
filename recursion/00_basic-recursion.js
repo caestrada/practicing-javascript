6a7b5b674429025278f561dce81563afd4eada75
Carlos A. Estrada <caestrada@users.noreply.github.com>
2020-08-24 08:37:31 -0400

    Update 00_basic-recursion.js

--- recursion/00_basic-recursion.js
+++ recursion/00_basic-recursion.js
@@ -1,10 +1,13 @@
 let wr = (msg='--------') => document.write(`<br>${msg}`);
 
 function basicRecursion(max, current) {
+  // Base Case
   if (current > max) return;
+  
   wr(current);
+  
+  // Recursion
   basicRecursion(max, current+1);
 }
 
 basicRecursion(5,1);
-wr();
