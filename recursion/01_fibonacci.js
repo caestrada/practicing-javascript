eeed1bf22fe242d04c017c8872cdb655be8b4d70
Carlos A. Estrada <caestrada@users.noreply.github.com>
2020-08-24 08:48:09 -0400

    Update 01_fibonacci.js

--- recursion/01_fibonacci.js
+++ recursion/01_fibonacci.js
@@ -1,1 +1,14 @@
+function fibonacci(n) {
+  // Base Case
+  if (n <= 2) { return 1; }
+  
+  // Recursive Case
+  else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
 
+
+for (let i = 1; i <= 20; i++) {
+  wr(`${i}. ${fibonacci(i)}`);
+}
