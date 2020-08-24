af7fa872a280982c74887f6027d239f4c73b1d12
Carlos A. Estrada <caestrada@users.noreply.github.com>
2020-08-24 08:36:22 -0400

    Create 00_basic-recursion.js

---
+++ recursion/00_basic-recursion.js
@@ -0,0 +1,10 @@
+let wr = (msg='--------') => document.write(`<br>${msg}`);
+
+function basicRecursion(max, current) {
+  if (current > max) return;
+  wr(current);
+  basicRecursion(max, current+1);
+}
+
+basicRecursion(5,1);
+wr();
