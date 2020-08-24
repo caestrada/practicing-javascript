7e8d7e1f2078ab785a4de043c542873db5a93b5f
Carlos A. Estrada <caestrada@users.noreply.github.com>
2020-08-24 08:49:13 -0400

    Update 01_fibonacci.js

--- recursion/01_fibonacci.js
+++ recursion/01_fibonacci.js
@@ -8,7 +8,10 @@ function fibonacci(n) {
   }
 }
 
+// Adding to DOM
+let wr = (msg='--------') => document.write(`<br>${msg}`);
 
+// Calling fibonacci
 for (let i = 1; i <= 20; i++) {
   wr(`${i}. ${fibonacci(i)}`);
 }
