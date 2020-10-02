67bc902d65bb5319992dc69bc4373f66c1c63960
Carlos Estrada <carlos.estrada@piikaaboo.com>
2020-10-02 10:06:46 -0400

    Update binary search

--- general/binary-search.js
+++ general/binary-search.js
@@ -1,21 +1,25 @@
 function binarySearch(list, item) {
-    let min = 0;
-    let max = list.length - 1;
+    let start = 0;
+    let end = list.length - 1;
     let i;
 
-    while (min < max) {
-        i = Math.floor((min + max) / 2);
+    while (start <= end) {
+        i = Math.floor((start + end) / 2);
 
         if (list[i] === item) {
             return i;
         } else {
             if (list[i] < item) {
-                min = i + 1;
+                start = i + 1;
             } else {
-                max = i - 1;
+                end = i - 1;
             }
         }
     }
 
     return -1;
 }
+
+// Test
+const l = [1, 2, 3, 4, 5];
+console.log(binarySearch(l, 5));
